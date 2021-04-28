4ae47ed3ae38381368225adef6cd64fd10a86af7
Albert Ramirez <albert.ramirez@bewica.com>
2021-04-28 08:51:02 +0200

    support for working-directory

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const core = require('@actions/core');
 const { spawn } = require("child_process");
 
-const run = (cmd, onError) => {
-    const c = spawn(cmd, { shell: true });
+const run = (cmd, cwd, onError) => {
+    const c = spawn(cmd, { shell: true, cwd: cwd });
     c.stdout.on("data", data => { console.log(`[${c.pid}] stdout: ${data}`); });
     c.stderr.on("data", data => { console.log(`[${c.pid}] stderr: ${data}`); });
     c.on('error', (error) => { onError(`[${c.pid || "no pid"}] err: ${error.message}`) });
@@ -13,10 +13,11 @@ const run = (cmd, onError) => {
 }
 
 const commands = core.getInput('commands');
+const cwd = core.getInput('working-directory') || undefined;
 const cmds = commands
     .trim().replace(/^\[+/, "").replace(/\]+$/, "")
     .split(",")
     .map((s) => s.trim()).filter((s) => s)
 for (const cmd of cmds) {
-    run(cmd, core.setFailed);
+    run(cmd, cwd, core.setFailed);
 }
