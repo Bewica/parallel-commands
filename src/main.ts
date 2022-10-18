33500079a6e841e9cbac5e7c59de5f97b5552ab7
Ivan Gavrilov <ivan.gavrilov@factum.es>
2022-10-18 11:05:24 +0200

    Add test logging

--- src/main.ts
+++ src/main.ts
@@ -23,6 +23,7 @@ const run = (
 }
 
 const commands: string = core.getInput('commands')
+console.log(`commands: ${commands}`)
 const cwd = core.getInput('working-directory') || undefined
 const cmds = commands
   .trim()
